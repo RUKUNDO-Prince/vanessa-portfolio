7330d23ea2816b5bbce65aadce842ffa4cc2e02e
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-09-24 10:39:23 +0300

    finished the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-between'>
-        <p>Copywrite &copy; 2022 | All rights reserved</p>
-        <p>Vanessa UWASE</p>
+    <div className='flex flex-col md:flex-row justify-between bg-purple-primary p-6 text-white'>
+        <p className='font-exo2 font-semibold md:text-[1.2em]'>Copywrite &copy; 2022 | All rights reserved</p>
+        <p className='font-exo2 font-semibold md:text-[1.2em]'>Vanessa UWASE</p>
     </div>
   )
 }
