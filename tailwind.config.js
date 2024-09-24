ef7cdb2fb1eaa537ff53a36ad5e8a8ce38f0b93f
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-09-24 10:34:45 +0300

    added some custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+    fontFamily: {
+      lato: ["Lato", "sans-serif"],
+      inter: ["inter", "sans-serif"],
+      poppins: ["poppins", "sans-serif"],
+      libre: ["Libre Baskerville", "serif"],
+      exo2: ["Exo 2", "sans-serif"],
+    },
+    colors: {
+      primary: "#F86F03",
+      secondary: "#",
+      white: "#FFFFFF",
+      blue: {
+        primary: "#170550",
+        secondary: "#43366A",
+      },
+      purple: {
+        primary: "#31065A"
+      },
+    },
     extend: {},
   },
   plugins: [],
