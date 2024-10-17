3921019cdde0a744bb1d29ea72c0dbe5ff9d2058
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-10-17 20:45:20 +0300

    rendered the gallery images

--- src/assets/index.js
+++ src/assets/index.js
@@ -34,6 +34,25 @@ import nina from './testimonials/nina.png'
 import tresol from './testimonials/tresol.png'
 import awet from './testimonials/awet.png'
 import hope from './testimonials/hope.png'
+import gallery1 from './gallery/1.jpg'
+import gallery2 from './gallery/2.jpg'
+import gallery3 from './gallery/3.jpg'
+import gallery4 from './gallery/4.jpg'
+import gallery5 from './gallery/5.jpg'
+import gallery6 from './gallery/6.jpg'
+import gallery7 from './gallery/7.jpg'
+import gallery8 from './gallery/8.jpg'
+import gallery9 from './gallery/9.jpg'
+import gallery10 from './gallery/10.jpg'
+import gallery11 from './gallery/11.jpg'
+import gallery12 from './gallery/12.jpg'
+import gallery13 from './gallery/13.jpg'
+import gallery14 from './gallery/14.jpg'
+import gallery15 from './gallery/15.jpg'
+import gallery16 from './gallery/16.jpg'
+import gallery17 from './gallery/17.jpg'
+import gallery18 from './gallery/18.jpg'
+import gallery19 from './gallery/19.jpg'
 
 export {
     logo,
@@ -71,5 +90,24 @@ export {
     hope,
     nina,
     tresol,
-    awet
+    awet,
+    gallery1,
+    gallery2,
+    gallery3,
+    gallery4,
+    gallery5,
+    gallery6,
+    gallery7,
+    gallery8,
+    gallery9,
+    gallery10,
+    gallery11,
+    gallery12,
+    gallery13,
+    gallery14,
+    gallery15,
+    gallery16,
+    gallery17,
+    gallery18,
+    gallery19,
 }
