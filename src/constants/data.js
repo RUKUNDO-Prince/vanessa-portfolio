adbd1642e941a0cd784d5c50c4a9a8d87c7983e8
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-10-17 20:49:06 +0300

    added the gallery images

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,180 +1,306 @@
-import { facebook, instagram, linkedin, github, backend, embedded, design, css, js, php, ruby, java, figma, cpp, c, html, python, golang, npm, project1, project2, project3, project4, img, tresol, hope, awet, nina } from "../assets"
+import {
+  facebook,
+  instagram,
+  linkedin,
+  github,
+  backend,
+  embedded,
+  design,
+  css,
+  js,
+  php,
+  ruby,
+  java,
+  figma,
+  cpp,
+  c,
+  html,
+  python,
+  golang,
+  npm,
+  project1,
+  project2,
+  project3,
+  project4,
+  img,
+  tresol,
+  hope,
+  awet,
+  nina,
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  gallery9,
+  gallery10,
+  gallery11,
+  gallery12,
+  gallery13,
+  gallery14,
+  gallery15,
+  gallery16,
+  gallery17,
+  gallery18,
+  gallery19,
+} from "../assets";
 
 export const socialLinks = [
-    {
-        icon: facebook,
-        link: "#"
-    },
-    {
-        icon: linkedin,
-        link: "https://www.linkedin.com/in/uwase-vanessa-607243256/"
-    },
-    {
-        icon: github,
-        link: "#"
-    },
-    {
-        icon: instagram,
-        link: "https://www.instagram.com/van_eus/"
-    },
-]
+  {
+    icon: facebook,
+    link: "#",
+  },
+  {
+    icon: linkedin,
+    link: "https://www.linkedin.com/in/uwase-vanessa-607243256/",
+  },
+  {
+    icon: github,
+    link: "#",
+  },
+  {
+    icon: instagram,
+    link: "https://www.instagram.com/van_eus/",
+  },
+];
 
 export const servicesData = [
-    {
-        icon: backend,
-        title: "Backend Developer",
-        desc: "I specialize in building scalable and robust server-side applications that ensure seamless communication between the front end and databases."
-    },
-    {
-        icon: embedded,
-        title: "Embedded Systems",
-        desc: "With experience in designing and developing embedded systems, I provide end-to-end solutions that integrate hardware and software, ensuring efficient performance and functionality for real-world applications."
-    },
-    {
-        icon: design,
-        title: "UI/UX Design",
-        desc: "I focus on creating intuitive and user-centered designs that enhance the user experience. From wireframes to prototypes, my design process is data-driven and ensures both visual appeal and functionality"
-    },
-    {
-        icon: backend,
-        title: "Product Designer",
-        desc: "As a product designer, I blend creativity with functionality to design products that solve real-world problems. I work closely with clients to transform their ideas into tangible, user-friendly products."
-    },
-    {
-        icon: embedded,
-        title: "Researcher",
-        desc: "I conduct thorough and methodical research to provide actionable insights. Whether it's market analysis, user research, or technology trends, my findings help clients make informed decisions and innovate effectively."
-    },
-    {
-        icon: design,
-        title: "IoT Developer",
-        desc: "I develop Internet of Things (IoT) solutions that connect physical devices to the digital world. My expertise covers sensor integration, connectivity, and cloud-based applications to enhance automation and data analytics."
-    },
-]
+  {
+    icon: backend,
+    title: "Backend Developer",
+    desc: "I specialize in building scalable and robust server-side applications that ensure seamless communication between the front end and databases.",
+  },
+  {
+    icon: embedded,
+    title: "Embedded Systems",
+    desc: "With experience in designing and developing embedded systems, I provide end-to-end solutions that integrate hardware and software, ensuring efficient performance and functionality for real-world applications.",
+  },
+  {
+    icon: design,
+    title: "UI/UX Design",
+    desc: "I focus on creating intuitive and user-centered designs that enhance the user experience. From wireframes to prototypes, my design process is data-driven and ensures both visual appeal and functionality",
+  },
+  {
+    icon: backend,
+    title: "Product Designer",
+    desc: "As a product designer, I blend creativity with functionality to design products that solve real-world problems. I work closely with clients to transform their ideas into tangible, user-friendly products.",
+  },
+  {
+    icon: embedded,
+    title: "Researcher",
+    desc: "I conduct thorough and methodical research to provide actionable insights. Whether it's market analysis, user research, or technology trends, my findings help clients make informed decisions and innovate effectively.",
+  },
+  {
+    icon: design,
+    title: "IoT Developer",
+    desc: "I develop Internet of Things (IoT) solutions that connect physical devices to the digital world. My expertise covers sensor integration, connectivity, and cloud-based applications to enhance automation and data analytics.",
+  },
+];
 
 export const skillsData = [
-    {
-        icon: css,
-        desc: "CSS"
-    },
-    {
-        icon: js,
-        desc: "JS"
-    },
-    {
-        icon: php,
-        desc: "PHP"
-    },
-    {
-        icon: ruby,
-        desc: "Ruby"
-    },
-    {
-        icon: java,
-        desc: "Java"
-    },
-    {
-        icon: figma,
-        desc: "Figma"
-    },
-    {
-        icon: cpp,
-        desc: "C++"
-    },
-    {
-        icon: c,
-        desc: "C"
-    },
-    {
-        icon: html,
-        desc: "HTML"
-    },
-    {
-        icon: python,
-        desc: "Python"
-    },
-    {
-        icon: golang,
-        desc: "Go Lang"
-    },
-    {
-        icon: npm,
-        desc: "NPM"
-    },
-]
+  {
+    icon: css,
+    desc: "CSS",
+  },
+  {
+    icon: js,
+    desc: "JS",
+  },
+  {
+    icon: php,
+    desc: "PHP",
+  },
+  {
+    icon: ruby,
+    desc: "Ruby",
+  },
+  {
+    icon: java,
+    desc: "Java",
+  },
+  {
+    icon: figma,
+    desc: "Figma",
+  },
+  {
+    icon: cpp,
+    desc: "C++",
+  },
+  {
+    icon: c,
+    desc: "C",
+  },
+  {
+    icon: html,
+    desc: "HTML",
+  },
+  {
+    icon: python,
+    desc: "Python",
+  },
+  {
+    icon: golang,
+    desc: "Go Lang",
+  },
+  {
+    icon: npm,
+    desc: "NPM",
+  },
+];
 
 export const projectsData = [
-    {
-        icon: project1,
-        name: "Novars - Agri-Based",
-        link: "http://194.163.167.131:3030/  ----- Novars "
-    },
-    {
-        icon: project2,
-        name: "Libblio - Library Management System",
-        link: "https://www.libblio.com/"
-    },
-    {
-        icon: project3,
-        name: "CrytaVita  -  Disaster Management",
-        link: "https://cryptavita.vercel.app/"
-    },
-    {
-        icon: project4,
-        name: "Inverwa - Agency Web",
-        link: "#"
-    },
-]
+  {
+    icon: project1,
+    name: "Novars - Agri-Based",
+    link: "http://194.163.167.131:3030/  ----- Novars ",
+  },
+  {
+    icon: project2,
+    name: "Libblio - Library Management System",
+    link: "https://www.libblio.com/",
+  },
+  {
+    icon: project3,
+    name: "CrytaVita  -  Disaster Management",
+    link: "https://cryptavita.vercel.app/",
+  },
+  {
+    icon: project4,
+    name: "Inverwa - Agency Web",
+    link: "#",
+  },
+];
 
 export const carouselData = [
-    {
-        index: 1,
-        name: "Remy Tresol",
-        img: tresol,
-        position: "Co-CEO & Co-Founder of Libblio",
-        desc: "Vanessa is a backend wizard! She transformed our system, making it faster, more secure, and scalable. Her ability to solve complex problems is unmatched. Highly recommend for any backend project"
-    },
-    {
-        index: 2,
-        name: "Hope Iradukunda",
-        img: hope,
-        position: "Front-end  developer of Novars",
-        desc: "Vanessa, as the CEO of Novars, is an outstanding leader. Her vision, technical expertise, and ability to motivate the team have been key to our project's success"
-    },
-    {
-        index: 3,
-        name: "Dr. Awet Fesseha",
-        img: awet,
-        position: "Instructor of Software Engineering",
-        desc: "Vanessa consistently demonstrates exceptional skill and creativity in software engineering. Her passion for learning and ability to tackle complex challenges make her a remarkable talent in the field."
-    },
-    {
-        index: 3,
-        name: "Nina Henriette",
-        img: nina,
-        position: "Project Manager of ElecSync",
-        desc: "Her UI/UX design skills are top-notch. She created an intuitive, visually stunning interface that significantly improved user engagement. The entire experience was seamless from start to finish"
-    },
-]
+  {
+    index: 1,
+    name: "Remy Tresol",
+    img: tresol,
+    position: "Co-CEO & Co-Founder of Libblio",
+    desc: "Vanessa is a backend wizard! She transformed our system, making it faster, more secure, and scalable. Her ability to solve complex problems is unmatched. Highly recommend for any backend project",
+  },
+  {
+    index: 2,
+    name: "Hope Iradukunda",
+    img: hope,
+    position: "Front-end  developer of Novars",
+    desc: "Vanessa, as the CEO of Novars, is an outstanding leader. Her vision, technical expertise, and ability to motivate the team have been key to our project's success",
+  },
+  {
+    index: 3,
+    name: "Dr. Awet Fesseha",
+    img: awet,
+    position: "Instructor of Software Engineering",
+    desc: "Vanessa consistently demonstrates exceptional skill and creativity in software engineering. Her passion for learning and ability to tackle complex challenges make her a remarkable talent in the field.",
+  },
+  {
+    index: 3,
+    name: "Nina Henriette",
+    img: nina,
+    position: "Project Manager of ElecSync",
+    desc: "Her UI/UX design skills are top-notch. She created an intuitive, visually stunning interface that significantly improved user engagement. The entire experience was seamless from start to finish",
+  },
+];
 
 // constants/data.js
 
 export const galleryData = [
-    {
-      id: 1,
-      image: project1,
-      title: 'Gallery Image 1',
-    },
-    {
-      id: 2,
-      image: project2,
-      title: 'Gallery Image 2',
-    },
-    {
-      id: 3,
-      image: project3,
-      title: 'Gallery Image 3',
-    },
-    // Add more gallery items here
-  ];
-  
+  {
+    id: 1,
+    image: gallery1,
+    title: "Gallery Image 1",
+  },
+  {
+    id: 2,
+    image: gallery2,
+    title: "Gallery Image 2",
+  },
+  {
+    id: 3,
+    image: gallery3,
+    title: "Gallery Image 3",
+  },
+  {
+    id: 4,
+    image: gallery4,
+    title: "Gallery Image 4",
+  },
+  {
+    id: 5,
+    image: gallery5,
+    title: "Gallery Image 5",
+  },
+  {
+    id: 6,
+    image: gallery6,
+    title: "Gallery Image 6",
+  },
+  {
+    id: 7,
+    image: gallery7,
+    title: "Gallery Image 7",
+  },
+  {
+    id: 8,
+    image: gallery8,
+    title: "Gallery Image 8",
+  },
+  {
+    id: 9,
+    image: gallery9,
+    title: "Gallery Image 9",
+  },
+  {
+    id: 10,
+    image: gallery10,
+    title: "Gallery Image 10",
+  },
+  {
+    id: 11,
+    image: gallery11,
+    title: "Gallery Image 11",
+  },
+  {
+    id: 12,
+    image: gallery12,
+    title: "Gallery Image 12",
+  },
+  {
+    id: 13,
+    image: gallery13,
+    title: "Gallery Image 13",
+  },
+  {
+    id: 14,
+    image: gallery14,
+    title: "Gallery Image 14",
+  },
+  {
+    id: 15,
+    image: gallery15,
+    title: "Gallery Image 15",
+  },
+  {
+    id: 16,
+    image: gallery16,
+    title: "Gallery Image 16",
+  },
+  {
+    id: 17,
+    image: gallery17,
+    title: "Gallery Image 17",
+  },
+  {
+    id: 18,
+    image: gallery18,
+    title: "Gallery Image 18",
+  },
+  {
+    id: 19,
+    image: gallery19,
+    title: "Gallery Image 19",
+  },
+];
