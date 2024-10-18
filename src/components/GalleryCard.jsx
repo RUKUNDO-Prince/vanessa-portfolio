e2c971543a4cd63ec825b6e113c80ccef01c7076
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-10-18 16:51:00 +0300

    some minor changes

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GalleryCard = ({ image, title }) => {
   return (
-    <div className="relative w-[450px] h-[300px] overflow-hidden">
+    <div className="relative w-[450px] h-[350px] overflow-hidden">
       <img src={image} alt={title} className="w-full h-full object-cover" />
       <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-60 text-white text-center">
         <h3 className="py-2">{title}</h3>
